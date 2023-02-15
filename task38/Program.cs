2e6afd4eeaf5be49e46ddda12a6e2b1ebdda95d5
«Natalia <kolodina_melbyk@mail.ru>
2023-02-15 20:06:56 +0400

    add task38

---
+++ task38/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+Console.Write("Введите количество элементов в массиве: ");
+int length = int.Parse(Console.ReadLine());
+
+double[] arr = GetArray(length);
+
+Console.Write("[");
+PrdoubleArray(arr);
+Console.Write("] -> ");
+Console.Write(MinMAx(Min(arr), Max(arr)));
+
+double[] GetArray(int size)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble()*100;
+    }
+    return res;
+}
+
+void PrdoubleArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double Max(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double MinMAx(double min, double max)
+{
+    double res = max - min;
+    return res;
+}
